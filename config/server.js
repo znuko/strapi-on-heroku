dfa9a0808ff082e1c839645a8b6d6e2c953d0dd7
znuko <nkymgenius@gmail.com>
2021-09-18 19:44:00 +0900

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cccc04315e839505bc0065358cd11c01'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '40a30537-28f8-411e-ad03-eba7a3991688'
+};
